0f5e6898c96a0529e474e59dd0e18afbeaf2565e
David Wagner <wagdav@gmail.com>
2024-08-12 16:23:45 +0200

    EXPERIMENT: Fix overlay for multiple Python versions

--- overlays/awslambdaric.nix
+++ overlays/awslambdaric.nix
@@ -1,18 +1,17 @@
-# Based on the example https://nixos.wiki/wiki/Overlays#Python_Packages_Overlay
+# https://nixos.org/manual/nixpkgs/stable/#how-to-override-a-python-package-for-all-python-versions-using-extensions
 final: prev:
-rec {
-  python3 = prev.python3.override
-    {
-      packageOverrides = final: prev: {
-        awslambdaric = prev.awslambdaric.overrideAttrs (old: {
+{
+  pythonPackageExtensions = prev.pythonPackageExtensions ++ [
+    (
+      python-final: python-prev: {
+        awslambdaric = python-prev.awslambdaric.overridePythonAttrs (old: {
           disabledTests = (old.disabledTests or [ ]) ++ [
             # Tests fail with "ValueError: Out of range float values are not JSON compliant"
             "test_to_json_decimal_encoding_nan"
             "test_to_json_decimal_encoding_negative_nan"
           ];
         });
-      };
-    };
-
-  python3Packages = python3.pkgs;
+      }
+    )
+  ];
 }
